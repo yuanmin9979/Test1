7c2ef70bcbd09fd838fb794da3f98ee03241fdb4
yuanmin <you@example.com>
2021-07-06 17:29:22 +0800

    first 提交测试

---
+++ 09Game.UserService/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _09Game.UserService.Models;
+
+namespace _09Game.UserService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Json("111");
+        }
+    }
+}
