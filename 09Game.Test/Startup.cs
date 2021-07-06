b829e9ead953886a9ebe4e57dc801246f048f5b8
e <1045231079@qq.com>
2021-07-06 18:30:54 +0800

    再次测试

--- 09Game.Test/Startup.cs
+++ 09Game.Test/Startup.cs
@@ -17,7 +17,6 @@ namespace _09Game.Test
             services.AddHttpReportsDashboard().AddMySqlStorage();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseHttpReportsDashboard();
